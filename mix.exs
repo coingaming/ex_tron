08baa559df42c5430c1d6cc50cbf1d98b7c6e7b4
syfgkjasdkn <kindly@from.network>
2018-10-29 13:55:30 +0000

    add docs

--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule Tron.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :tron,
-      version: "0.1.0",
-      elixir: "~> 1.7",
+      version: @version,
+      elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      consolidate_protocols: Mix.env() != :test,
+      deps: deps(),
+      description: description(),
+      package: package(),
+      docs: docs()
     ]
   end
 
@@ -27,7 +33,35 @@ defmodule Tron.MixProject do
       {:google_protos, "~> 0.1"},
       {:grpc, "~> 0.3.0-alpha.2"},
       {:keccakf1600, "~> 2.0", hex: :keccakf1600_orig},
-      {:libsecp256k1, "~> 0.1.10"}
+      {:libsecp256k1, "~> 0.1.10"},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    """
+    TRON protocol implementation in elixir.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["doijaps"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/syfgkjasdkn/tron-protocol"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      name: "Tron",
+      source_ref: "v#{@version}",
+      canonical: "http://hexdocs.pm/tron",
+      source_url: "https://github.com/syfgkjasdkn/tron-protocol",
+      extras: [
+        "README.md"
+      ]
     ]
   end
 end
